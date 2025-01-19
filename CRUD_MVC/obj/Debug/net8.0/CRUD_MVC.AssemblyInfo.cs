4eb1f3db199af157e23974a275612c5000cb1b70
Administrator <lavlesh08mey@gmail.com>
2025-01-19 18:19:05 +0530

    commit

--- CRUD_MVC/obj/Debug/net8.0/CRUD_MVC.AssemblyInfo.cs
+++ CRUD_MVC/obj/Debug/net8.0/CRUD_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84198e7f3753e8f17c9251f05808fb4cd5ed355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c8ea75d4c69baf814226388264dc4a379f4500")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
